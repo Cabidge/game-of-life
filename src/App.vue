92704a353d057bf100331c5f2bc0ca315257b02a
Cabidge <cabbageboi69@gmail.com>
2021-08-12 13:38:49 -0400

    Added options to change board and cell sizes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-    <GameBoard width="64" height="32" />
+    <input type="number" v-model="width" />
+    <input type="number" v-model="height" />
+    <input type="number" v-model="cellPx" />
+    <GameBoard :width="width" :height="height" :cellPx="cellPx" />
 </template>
 
 <script lang="ts">
@@ -10,6 +13,13 @@ export default defineComponent({
     name: "App",
     components: {
         GameBoard,
+    },
+    data() {
+        return {
+            width: 64,
+            height: 32,
+            cellPx: 16,
+        };
     },
 });
 </script>
